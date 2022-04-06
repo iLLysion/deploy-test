ef4700671fd9c751f0c9ca7ce1880ac343489937
Oleksandr Ivanotchak <chaksw@gmail.com>
2022-04-06 12:35:16 +0300

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,7 +14,7 @@ set :puma_workers,    2
 set :rbenv_type,      :user
 set :rbenv_ruby,      '2.5.1'
 set :rbenv_prefix,    "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins,  %w{rake gem bundle ruby rails}
+set :rbenv_map_bins,  %w{rake gem bundle ruby rails puma pumactl}
 set :rbenv_roles,     :all # default value
 
 # Don't change these unless you know what you're doing
